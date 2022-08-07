ca4d0fa552cc7baea586688cc66e4f51745c5d12
David <dav.camposc@gmail.com>
2022-08-07 14:16:00 -0500

    subir proyecto a main

---
+++ HolaMundo/src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Hola, Bienvenidos a Java!!");
+        System.out.println("Hola a Todos!!!");
+
+        int edad = 25;   // => Declaración e Inicialización
+        edad = edad  + 30; // => Asignación (cambiio de valor) + operacion
+
+        String nombre;   //Declaración
+        nombre = "Jean Karlo"; //Asginación
+
+
+        System.out.println("La edad es: " + edad);
+        System.out.println(nombre);
+
+    }
+}
